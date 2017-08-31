360943dc0a2091b589b5208735621aa4983c647d
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-08-31 14:33:45 -0700

    finished sprint 1 step 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,51 @@
  * into functions and objects as needed.
  *
  */
+ function renderAlbum(album) {
+   console.log(album);
+  $("#albums").append(`<div class="row album">
 
+   <div class="col-md-10 col-md-offset-1">
+     <div class="panel panel-default">
+       <div class="panel-body">
+
+
+       <!-- begin album internal row -->
+         <div class='row'>
+           <div class="col-md-3 col-xs-12 thumbnail album-art">
+             <img src="../public/images/800x800.png" alt="album image">
+           </div>
+
+           <div class="col-md-9 col-xs-12">
+             <ul class="list-group">
+               <li class="list-group-item">
+                 <h4 class='inline-header'>Album Name:</h4>
+                 <span class='album-name'>${album.name}</span>
+               </li>
+
+               <li class="list-group-item">
+                 <h4 class='inline-header'>Artist Name:</h4>
+                 <span class='artist-name'>${album.artistName}</span>
+               </li>
+
+               <li class="list-group-item">
+                 <h4 class='inline-header'>Released date:</h4>
+                 <span class='album-releaseDate'>${album.releaseDate}</span>
+               </li>
+             </ul>
+           </div>
+
+         </div>
+         <!-- end of album internal row -->
+
+         <div class='panel-footer'>
+         </div>
+
+       </div>
+     </div>
+   </div>
+ </div>`);
+}
 
 /* hard-coded data! */
 var sampleAlbums = [];
@@ -37,16 +81,15 @@ sampleAlbums.push({
 
 
 
-$(document).ready(function() {
-  console.log('app.js loaded!');
-});
-
-
-
-
 
 // this function takes a single album and renders it to the page
-function renderAlbum(album) {
-  console.log('rendering album:', album);
-
-}
+// function renderAlbum(album) {
+//   console.log('rendering album:', album)
+//   ;
+//
+// }
+$(document).ready(function() {
+  console.log('app.js loaded!');
+  sampleAlbums.forEach(function(album){
+  renderAlbum(album)});
+});
