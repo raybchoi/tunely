39f5f4c8bc086f99b5d1531b31ddda26acfc0fc2
raybchoi <raybchoi@gmail.com>
2017-08-31 14:53:50 -0700

    working database stuff

--- models/album.js
+++ models/album.js
@@ -1,2 +1,16 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+
+// creation of the album db
+var albumSchema = new Schema({
+  artistName: String,
+  name: String,
+  releaseDate: String,
+  genres: [String]
+})
+
+// creation of the model
+const Album = mongoose.model('Album', albumSchema);
+
+// export the model
+module.exports = Album;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,7 @@
 var mongoose = require("mongoose");
+// const Album = require('./album.js');
+
+
 mongoose.connect("mongodb://localhost/tunely");
+// module.exports = Album;
+module.exports.Album = require("./album.js");
