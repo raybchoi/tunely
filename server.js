4063b8e2c6e570a055a9e3abcdbee0d214a116d5
raybchoi <raybchoi@gmail.com>
2017-08-31 11:32:52 -0700

    Completed Sprint 0 - Step 2 & 3 - route for homepage and setup static / fix static routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// setting and config
 // require apps / thirdparty
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -6,9 +7,16 @@ const bodyParser = require('body-parser');
 // generate a new express app and call it 'app'
 const app = express();
 
+// serve static files in public
+app.use(express.static('public'))
+
+
+
 app.get('/', (req, res) => {
-  console.log('Yo! high-five broooooo!~!');
+  res.sendFile('views/index.html', { root: __dirname});
+  // __direname => /Users/raychoi/WDI/03_week3/03_03_lab_tunely
 });
+
 
 app.listen(3000, function() {
   console.log('Server 3000 is up and running...')
